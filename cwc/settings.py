a97813817ebfabcfbc524bad07cef10579382977
Jeffrey Chidi <chidij75@gmail.com>
2018-01-11 21:51:52 +0100

    edited allowed host

--- cwc/settings.py
+++ cwc/settings.py
@@ -30,7 +30,9 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&
 
 DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'citywt.herokuapp.com'
+]
 
 
 # Application definition
